63a47a91b761ac2e0fdcbfc2f5daf620e95f882f
Corentin Macé <corentin.mace@ynov.com>
2021-12-19 23:33:23 +0100

    Added Key Items

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -8,6 +8,7 @@ export default {
     const bows = ref([])
     const shields = ref([])
     const armors = ref([])
+    const keys = ref([])
     const lang = ref('')
 
     const showWeapons = ref(false)
@@ -20,6 +21,8 @@ export default {
     const showHeads = ref(false)
     const showBodies = ref(false)
     const showLegs = ref(false)
+    const showKeys = ref(false)
+    const showSaddles = ref(false)
 
     onMounted(() => {
       axios.get('api/api.json')
@@ -29,6 +32,7 @@ export default {
         bows.value = res.data.bows
         shields.value = res.data.shields
         armors.value = res.data.armors
+        keys.value = res.data.key_items
         lang.value = 'en'
       })
       .catch(err => console.log(err))
@@ -55,19 +59,23 @@ export default {
           showBodies.value = !showBodies.value
         } else if (div == "legs") {
           showLegs.value = !showLegs.value
+        } else if (div == "key-items") {
+          showKeys.value = !showKeys.value
+        } else if (div == "saddles") {
+          showSaddles.value = !showSaddles.value
         }
     }
 
-    return { weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows, shields, showShields, armors, showArmors, showHeads, showBodies, showLegs}
+    return { weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows, shields, showShields, armors, showArmors, showHeads, showBodies, showLegs, keys, showKeys, showSaddles}
   }
 }
 
 </script>
 
 <template>
-<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2" @click="toggleShow('weapons')">> Weapons</h1>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('weapons')">> Weapons</h1>
 <div v-if="showWeapons">
-  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('one-handed')">> One handed</h1>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('one-handed')">> One handed</h1>
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showOneHanded">
     <div class="flex justify-center items-center" v-for="(weapon, name) in weapons">
       <div class="flex flex-col items-center h-50 w-60" v-if="weapon.category == 'one-handed'">
@@ -76,7 +84,7 @@ export default {
       </div>
     </div>
   </div>
-  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('two-handed')">> Two handed</h1>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('two-handed')">> Two handed</h1>
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showTwoHanded">
     <div class="flex justify-center items-center" v-for="(weapon, name) in weapons">
       <div class="flex flex-col items-center h-50 w-60" v-if="weapon.category == 'two-handed'">
@@ -85,7 +93,7 @@ export default {
       </div>
     </div>
   </div>
-  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('spear')">> Spear</h1>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('spear')">> Spear</h1>
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showSpear">
     <div class="flex justify-center items-center" v-for="(weapon, name) in weapons">
       <div class="flex flex-col items-center h-50 w-60" v-if="weapon.category == 'spear'">
@@ -95,7 +103,7 @@ export default {
     </div>
   </div>
 </div>
-<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2" @click="toggleShow('bows')">> Bows</h1>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('bows')">> Bows</h1>
 <div v-if="showBows">
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showBows">
     <div class="flex justify-center items-center" v-for="(bows, name) in bows">
@@ -106,7 +114,7 @@ export default {
     </div>
   </div>
 </div>
-<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2" @click="toggleShow('shields')">> Shields</h1>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('shields')">> Shields</h1>
 <div v-if="showShields">
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showShields">
     <div class="flex justify-center items-center" v-for="(shields, name) in shields">
@@ -117,9 +125,9 @@ export default {
     </div>
   </div>
 </div>
-<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2" @click="toggleShow('armors')">> Armors</h1>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('armors')">> Armors</h1>
 <div v-if="showArmors">
-  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('heads')">> Head</h1>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('heads')">> Head</h1>
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showHeads">
     <div class="flex justify-center items-center" v-for="(armor, name) in armors">
       <div class="flex flex-col items-center h-50 w-60" v-if="armor.category == 'head'">
@@ -128,7 +136,7 @@ export default {
       </div>
     </div>
   </div>
-  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('bodies')">> Body</h1>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('bodies')">> Body</h1>
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showBodies">
     <div class="flex justify-center items-center" v-for="(armor, name) in armors">
       <div class="flex flex-col items-center h-50 w-60" v-if="armor.category == 'body'">
@@ -137,12 +145,32 @@ export default {
       </div>
     </div>
   </div>
-  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('legs')">> Legs</h1>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('legs')">> Legs</h1>
   <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showLegs">
     <div class="flex justify-center items-center" v-for="(armor, name) in armors">
       <div class="flex flex-col items-center h-50 w-60" v-if="armor.category == 'leg'">
           <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
           <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('key-items')">> Key Items</h1>
+<div v-if="showKeys">
+  <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5">
+    <div class="flex justify-center items-center" v-for="(key, name) in keys">
+      <div class="flex flex-col items-center h-50 w-60" v-if="key.category == 'key'">
+          <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="key.image">
+          <p class="py-2" v-if="key.names[lang]">{{ key.names[lang].replace(/_/g, ' ') }}</p>
+      </div>
+    </div>
+  </div>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('saddles')">> Saddles</h1>
+  <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showSaddles">
+    <div class="flex justify-center items-center" v-for="(key, name) in keys">
+      <div class="flex flex-col items-center h-50 w-60" v-if="key.category == 'saddles'">
+          <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="key.image">
+          <p class="py-2" v-if="key.names[lang]">{{ key.names[lang].replace(/_/g, ' ') }}</p>
       </div>
     </div>
   </div>
