0ac6af2cdc295beccdf7b8520a0ba5ff28a7fe46
Corentin Macé <corentin.mace@ynov.com>
2021-12-19 22:54:01 +0100

    Added Armor

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -7,6 +7,7 @@ export default {
     const weapons = ref([])
     const bows = ref([])
     const shields = ref([])
+    const armors = ref([])
     const lang = ref('')
 
     const showWeapons = ref(false)
@@ -15,6 +16,10 @@ export default {
     const showSpear = ref(false)
     const showBows = ref(false)
     const showShields = ref(false)
+    const showArmors = ref(false)
+    const showHeads = ref(false)
+    const showBodies = ref(false)
+    const showLegs = ref(false)
 
     onMounted(() => {
       axios.get('api/api.json')
@@ -23,6 +28,7 @@ export default {
         weapons.value = res.data.weapons
         bows.value = res.data.bows
         shields.value = res.data.shields
+        armors.value = res.data.armors
         lang.value = 'en'
       })
       .catch(err => console.log(err))
@@ -41,10 +47,18 @@ export default {
           showBows.value = !showBows.value
         } else if (div == "shields") {
           showShields.value = !showShields.value
+        } else if (div == "armors") {
+          showArmors.value = !showArmors.value
+        } else if (div == "heads") {
+          showHeads.value = !showHeads.value
+        } else if (div == "bodies") {
+          showBodies.value = !showBodies.value
+        } else if (div == "legs") {
+          showLegs.value = !showLegs.value
         }
     }
 
-    return { weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows, shields, showShields}
+    return { weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows, shields, showShields, armors, showArmors, showHeads, showBodies, showLegs}
   }
 }
 
@@ -99,6 +113,36 @@ export default {
       <div class="flex flex-col items-center h-50 w-60">
           <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="shields.image">
           <p class="py-2" v-if="shields.names[lang]">{{ shields.names[lang].replace(/_/g, ' ') }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2" @click="toggleShow('armors')">> Armors</h1>
+<div v-if="showArmors">
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('heads')">> Head</h1>
+  <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showHeads">
+    <div class="flex justify-center items-center" v-for="(armor, name) in armors">
+      <div class="flex flex-col items-center h-50 w-60" v-if="armor.category == 'head'">
+          <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
+          <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
+      </div>
+    </div>
+  </div>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('bodies')">> Body</h1>
+  <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showBodies">
+    <div class="flex justify-center items-center" v-for="(armor, name) in armors">
+      <div class="flex flex-col items-center h-50 w-60" v-if="armor.category == 'body'">
+          <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
+          <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
+      </div>
+    </div>
+  </div>
+  <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2" @click="toggleShow('legs')">> Legs</h1>
+  <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showLegs">
+    <div class="flex justify-center items-center" v-for="(armor, name) in armors">
+      <div class="flex flex-col items-center h-50 w-60" v-if="armor.category == 'leg'">
+          <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
+          <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
       </div>
     </div>
   </div>
