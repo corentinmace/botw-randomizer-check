8d9c18dfefcab002372fdc07ffd13aca767044ca
Corentin Macé <corentin.mace@ynov.com>
2021-12-19 20:18:19 +0100

    Added Bow

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -5,18 +5,21 @@ import axios from 'axios'
 export default {
   setup() {
     const weapons = ref([])
+    const bows = ref([])
     const lang = ref('')
 
     const showWeapons = ref(false)
     const showOneHanded = ref(false)
     const showTwoHanded = ref(false)
     const showSpear = ref(false)
+    const showBows = ref(false)
 
     onMounted(() => {
       axios.get('api/api.json')
       .then(res => {
         console.log(res.data)
         weapons.value = res.data.weapons
+        bows.value = res.data.bows
         lang.value = 'en'
       })
       .catch(err => console.log(err))
@@ -31,10 +34,12 @@ export default {
           showTwoHanded.value = !showTwoHanded.value
         } else if (div == "spear") {
           showSpear.value = !showSpear.value
+        } else if (div == "bows") {
+          showBows.value = !showBows.value
         }
     }
 
-    return { weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow,}
+    return { weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows}
   }
 }
 
@@ -67,6 +72,17 @@ export default {
       <div class="flex flex-col items-center h-50 w-60" v-if="weapon.category == 'spear'">
           <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="weapon.image">
           <p class="py-2" v-if="weapon.names[lang]">{{ weapon.names[lang].replace(/_/g, ' ') }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+<h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2" @click="toggleShow('bows')">> Bows</h1>
+<div v-if="showBows" @click="toggleShow('one-handed')">
+  <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showBows">
+    <div class="flex justify-center items-center" v-for="(bows, name) in bows">
+      <div class="flex flex-col items-center h-50 w-60">
+          <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="bows.image">
+          <p class="py-2" v-if="bows.names[lang]">{{ bows.names[lang].replace(/_/g, ' ') }}</p>
       </div>
     </div>
   </div>
