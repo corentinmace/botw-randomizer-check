61dfefc842f31aad894f3412d239f4cd0be4bed4
Corentin Macé <corentin.mace@ynov.com>
2021-12-22 17:44:41 +0100

    Added Materials/Food/Enemies Removed 's on images, fixed execption where the image has a 's in json

--- app/src/components/ItemList.vue
+++ app/src/components/ItemList.vue
@@ -10,6 +10,8 @@ export default {
     const armors = ref([])
     const keys = ref([])
     const runes = ref([])
+    const materials = ref([])
+    const enemies = ref([])
     const objectives = ref([])
     const lang = ref('')
     
@@ -26,6 +28,10 @@ export default {
     const showKeys = ref(false)
     const showSaddles = ref(false)
     const showRunes = ref(false)
+    const showMaterials = ref(false)
+    const showEnemies = ref(false)
+    const showFood = ref(false)
+    const showCookedFood = ref(false)
 
     onMounted(() => {
       axios.get('api/api.json')
@@ -37,6 +43,8 @@ export default {
         armors.value = res.data.armors
         keys.value = res.data.key_items
         runes.value = res.data.runes
+        materials.value = res.data.materials
+        enemies.value = res.data.enemies
         lang.value = 'en'
       })
       .catch(err => console.log(err))
@@ -69,7 +77,16 @@ export default {
           showSaddles.value = !showSaddles.value
         } else if (div == "runes") {
           showRunes.value = !showRunes.value
+        } else if (div == "materials") {
+          showMaterials.value = !showMaterials.value
+        } else if (div == "enemies") {
+          showEnemies.value = !showEnemies.value
+        } else if (div == 'food') {
+          showFood.value = !showFood.value
+        } else if (div == "coed-food") {
+          showCookedFood.value = !showCookedFood.value
         }
+        
     }
 
     const addToObjectives = (objetive) => {
@@ -82,15 +99,15 @@ export default {
 
     const completeObjectives = (e) => {
       console.log(e)
-      if(e.srcElement.classList.contains('bg-emerald-600')) {
-        e.srcElement.classList.remove('bg-emerald-600')
+      if(e.srcElement.classList.contains('grayscale')) {
+        e.srcElement.classList.remove('grayscale')
       } else {
-        e.srcElement.classList.add('bg-emerald-600')
+        e.srcElement.classList.add('grayscale')
       }
     }
 
 
-    return { completeObjectives, addToObjectives, removeFromObjectives, weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows, shields, showShields, armors, showArmors, showHeads, showBodies, showLegs, keys, showKeys, showSaddles, runes, showRunes, objectives}
+    return { completeObjectives, addToObjectives, removeFromObjectives, weapons, lang, showWeapons, showOneHanded, showTwoHanded, showSpear, toggleShow, bows, showBows, shields, showShields, armors, showArmors, showHeads, showBodies, showLegs, keys, showKeys, showSaddles, runes, showRunes, objectives, materials, showMaterials, enemies, showEnemies, showFood, showCookedFood}
   }
 }
 
@@ -103,7 +120,15 @@ export default {
          <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5">
              <div class="flex justify-center items-center" v-for="(objective, id) in objectives">
                  <div class="flex flex-col items-center w-56" @dblclick="removeFromObjectives(id) ">
-                     <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="objective.image" @click="completeObjectives($event)">
+                     <div class="border p-3 rounded w-20 h-20 grayscale">
+                        <div class="w-full h-full" @click="completeObjectives($event)" :style="{
+                            backgroundImage: 'url(' + objective.image.replace('\'', '') +')',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'contain',
+                            backgroundPosition: 'center'
+                          }">
+                        </div>
+                     </div>
                      <p class="py-2" v-if="objective.names[lang]">{{ objective.names[lang].replace(/_/g, ' ') }}</p>
                  </div>
              </div>
@@ -116,7 +141,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showOneHanded">
        <div class="flex justify-center items-center" v-for="(weapon, name) in weapons">
          <div class="flex flex-col items-center w-60" v-if="weapon.category == 'one-handed'" @click="addToObjectives(weapon)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="weapon.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+               <div class="w-full h-full" :style="{
+                        backgroundImage: 'url(' + weapon.image.replace('\'', '') +')',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center'
+                       }">
+               </div>
+             </div>
              <p class="py-2" v-if="weapon.names[lang]">{{ weapon.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -125,7 +158,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showTwoHanded">
        <div class="flex justify-center items-center" v-for="(weapon, name) in weapons">
          <div class="flex flex-col items-center w-60" v-if="weapon.category == 'two-handed'" @click="addToObjectives(weapon)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="weapon.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                <div class="w-full h-full" :style="{
+                  backgroundImage: 'url(' + weapon.image.replace('\'', '') +')',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'contain',
+                  backgroundPosition: 'center'
+                  }">
+               </div>
+             </div>
              <p class="py-2" v-if="weapon.names[lang]">{{ weapon.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -134,7 +175,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showSpear">
        <div class="flex justify-center items-center" v-for="(weapon, name) in weapons">
          <div class="flex flex-col items-center w-60" v-if="weapon.category == 'spear'" @click="addToObjectives(weapon)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="weapon.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + weapon.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="weapon.names[lang]">{{ weapon.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -145,7 +194,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showBows">
        <div class="flex justify-center items-center" v-for="(bow, name) in bows">
          <div class="flex flex-col items-center w-60" @click="addToObjectives(bow)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="bow.image">
+                <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + bow.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="bow.names[lang]">{{ bow.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -156,7 +213,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showShields">
        <div class="flex justify-center items-center" v-for="(shield, name) in shields">
          <div class="flex flex-col items-center w-60" @click="addToObjectives(shield)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="shield.image">
+                <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + shield.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="shield.names[lang]">{{ shield.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -168,7 +233,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showHeads">
        <div class="flex justify-center items-center" v-for="(armor, name) in armors">
          <div class="flex flex-col items-center w-60" v-if="armor.category == 'head'" @click="addToObjectives(armor)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + armor.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -177,7 +250,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showBodies">
        <div class="flex justify-center items-center" v-for="(armor, name) in armors">
          <div class="flex flex-col items-center w-60" v-if="armor.category == 'body'" @click="addToObjectives(armor)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + armor.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -186,7 +267,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showLegs">
        <div class="flex justify-center items-center" v-for="(armor, name) in armors">
          <div class="flex flex-col items-center w-60" v-if="armor.category == 'leg'" @click="addToObjectives(armor)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="armor.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + armor.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="armor.names[lang]">{{ armor.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -197,7 +286,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5">
        <div class="flex justify-center items-center" v-for="(key, name) in keys">
          <div class="flex flex-col items-center w-60" v-if="key.category == 'key'" @click="addToObjectives(key)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="key.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + key.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="key.names[lang]">{{ key.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -206,7 +303,15 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showSaddles">
        <div class="flex justify-center items-center" v-for="(key, name) in keys">
          <div class="flex flex-col items-center w-60" v-if="key.category == 'saddles'" @click="addToObjectives(key)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="key.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + key.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="key.names[lang]">{{ key.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
@@ -217,8 +322,90 @@ export default {
      <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showRunes">
        <div class="flex justify-center items-center" v-for="(rune, name) in runes">
          <div class="flex flex-col items-center w-60" @click="addToObjectives(rune)">
-             <img class="border p-3 bg-gray-600 rounded w-20 h-20" :src="rune.image">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + rune.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
              <p class="py-2" v-if="rune.names[lang]">{{ rune.names[lang].replace(/_/g, ' ') }}</p>
+         </div>
+       </div>
+     </div>
+   </div>
+   <!-- Materials / Food here -->
+   <h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('materials')">> Materials</h1>
+   <div v-if="showMaterials">
+     <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5">
+       <div class="flex justify-center items-center" v-for="(material, name) in materials">
+         <div class="flex flex-col items-center w-60" v-if="material.category == 'material'" @click="addToObjectives(material)">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + material.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
+             <p class="py-2" v-if="material.names[lang]">{{ material.names[lang].replace(/_/g, ' ') }}</p>
+         </div>
+       </div>
+     </div>
+     <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('food')">> Food</h1>
+     <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showFood">
+       <div class="flex justify-center items-center" v-for="(material, name) in materials">
+         <div class="flex flex-col items-center w-60" v-if="material.category == 'food'" @click="addToObjectives(material)">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + material.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
+             <p class="py-2" v-if="material.names[lang]">{{ material.names[lang].replace(/_/g, ' ') }}</p>
+         </div>
+       </div>
+     </div>
+     <h1 class="bg-gray-800 text-left text-m font-bold text-white px-10 py-2 border-solid border-b border-gray-600" @click="toggleShow('cooked-food')">> Cooked Food</h1>
+     <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showCookedFood">
+       <div class="flex justify-center items-center" v-for="(material, name) in materials">
+         <div class="flex flex-col items-center w-60" v-if="material.category == 'cooked-food'" @click="addToObjectives(material)">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + material.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
+             <p class="py-2" v-if="material.names[lang]">{{ material.names[lang].replace(/_/g, ' ') }}</p>
+         </div>
+       </div>
+     </div>
+   </div>
+
+  <h1 class="bg-gray-600 text-left text-m font-bold text-white px-5 py-2 border-solid border-b border-gray-800" @click="toggleShow('enemies')">> Enemies</h1>
+   <div v-if="showEnemies">
+     <div class="flex flex-row flex-wrap bg-gray-800 w-full text-white py-5" v-if="showEnemies">
+       <div class="flex justify-center items-center" v-for="(enemy, name) in enemies">
+         <div class="flex flex-col items-center w-60" @click="addToObjectives(enemy)">
+             <div class="border p-3 bg-gray-600 rounded w-20 h-20">
+                  <div class="w-full h-full" :style="{
+                    backgroundImage: 'url(' + enemy.image.replace('\'', '') +')',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                    backgroundPosition: 'center'
+                    }">
+                  </div>
+               </div>
+             <p class="py-2" v-if="enemy.names[lang]">{{ enemy.names[lang].replace(/_/g, ' ') }}</p>
          </div>
        </div>
      </div>
